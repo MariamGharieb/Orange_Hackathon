367d38ff9868bc73a873f40e0cbb78e02c8300be
MariamGharieb <60156809+MariamGharieb@users.noreply.github.com>
2022-10-05 01:52:32 +0200

    job Db commit

--- src/main/java/com/example/odc/Job.java
+++ src/main/java/com/example/odc/Job.java
@@ -1,4 +1,27 @@
 package com.example.odc;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table
 public class Job {
+    @SequenceGenerator(
+            name="job_sequence",
+            sequenceName = "job_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "job_sequence"
+    )
+    @Id
+    private int jobID;
+    private String jobName;
+    private String organizationName;
+    private String JobDescription;
+    private float salary;
+    @ManyToMany
+    ArrayList <Course> requiredCourses;
+
 }
